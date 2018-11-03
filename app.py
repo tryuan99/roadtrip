eaeb2a2c86488cc5a65f52370a3754701251f636
tryuan99 <tryuan99@gmail.com>
2018-11-03 14:13:34 -0700

    Added login and register

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import *
+from hashlib import sha256
 import database, uuid
 
 app = Flask(__name__)
@@ -6,6 +7,35 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def index():
 	return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	if request.method == 'POST':
+		username = request.form['username']
+		password = request.form['password']
+
+
+		
+		return redirect(url_for('trips'))
+
+	return render_template('login.html')
+
+@app.route('/logout', methods=['GET'])
+def logout():
+	return 'NOT IMPLEMENTED'
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+	if request.method == 'POST':
+		username = request.form['username']
+		password = request.form['password']
+		salt = uuid.uuid4().hex
+
+		hashed_password = sha256((password + salt).encode()).hexdigest()
+
+		database.execute('INSERT INTO users VALUES ("{}", "{}", "{}")'.format(username, hashed_password, salt))
+		return render_template('register.html', success='User registered successfully')
+	return render_template('register.html')
 
 @app.route('/trips', methods=['GET', 'POST'])
 def trips():
@@ -15,9 +45,9 @@ def trips():
 		origin = request.form['origin']
 		destination = request.form['destination']
 
-		database.execute('INSERT INTO trips VALUES ({}, {}, {}, {})'.format(id, username, origin, destination))
-		return render_template('trip_list.html')
-	
+		database.execute('INSERT INTO trips VALUES ("{}", "{}", "{}", "{}")'.format(id, username, origin, destination))
+		return render_template('trip_list', success='Trip planned successfully')
+
 	return render_template('trip_list.html')
 
 @app.route('/trips/new', methods=['GET'])
--- database.py
+++ database.py
@@ -4,23 +4,38 @@ import sys
 DATABASE_FILE = ':memory:'
 conn = sqlite3.connect(DATABASE_FILE)
 
+def logger(f):
+    def with_logger(*args, **kwargs):
+        print("Executing query", *args, file=sys.stderr)
+        try:
+            return f(*args, **kwargs)
+        except sqlite3.Error as e:
+            print("SQL Error Occurred:", e.args[0], file=sys.stderr)
+    return with_logger
+
+@logger
 def execute(query):
     with conn:
         conn.executescript(query)
 
+@logger
 def fetchone(query):
     with conn:
         return conn.execute(query).fetchone()
 
+@logger
 def fetchall(query):
     with conn:
         return conn.execute(query).fetchall()
 
 def init_database():
     conn.executescript("""
-    CREATE TABLE users (username text PRIMARY KEY UNIQUE NOT NULL);
+    CREATE TABLE users (username text PRIMARY KEY UNIQUE NOT NULL, password text NOT NULL, salt text NOT NULL);
+    INSERT INTO users VALUES ('test', '52d42704a5b54ab6800cc2385c60c6dc62e80da8e4a98b3c6a28dd1fcaa6015a', 'e5db57d0fd6c4e6fbc4ace161beb855f');
         
-    CREATE TABLE trips (id uuid PRIMARY KEY UNIQUE NOT NULL, username text NOT NULL, origin text NOT NULL, destination text NOT NULL);
+    CREATE TABLE trips (id uuid PRIMARY KEY UNIQUE NOT NULL, username text NOT NULL, origin text NOT NULL, destination text NOT NULL, seats int NOT NULL);
+
+    CREATE TABLE carpools (id uuid NOT NULL, username text NOT NULL);
     """)
     conn.commit()
 
