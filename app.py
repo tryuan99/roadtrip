806285a7213933234bfe0d5f9e97150a7f29895f
Jove Yuan <jyuan.twin1@gmail.com>
2018-11-03 15:06:36 -0700

    Code cleanup

--- app.py
+++ app.py
@@ -1,73 +1,83 @@
 from flask import *
 from hashlib import sha256
-import config, database, uuid
+import config
+import database
+import uuid
 
 app = Flask(__name__)
 app.secret_key = config.SECRET_KEY
 
+
 @app.route('/', methods=['GET'])
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	if request.method == 'POST':
-		username = request.form['username']
-		password = request.form['password']
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
 
-		record = database.fetchone('SELECT password, salt FROM users WHERE username="{}";'.format(username))
-		if not record:
-			return render_template('login.html', error='Invalid username or password')
+        record = database.fetchone('SELECT password, salt FROM users WHERE username="{}";'.format(username))
+        if not record:
+            return render_template('login.html', error='Invalid username or password')
 
-		correct_password, salt = record
-		hashed_password = sha256((password + salt).encode()).hexdigest()
+        correct_password, salt = record
+        hashed_password = sha256((password + salt).encode()).hexdigest()
 
-		if hashed_password != correct_password:
-			return render_template('login.html', error='Invalid username or password')
+        if hashed_password != correct_password:
+            return render_template('login.html', error='Invalid username or password')
 
-		session['username'] = username
-		return redirect(url_for('trips'))
+        session['username'] = username
+        return redirect(url_for('trips'))
 
-	return render_template('login.html')
+    return render_template('login.html')
+
 
 @app.route('/logout', methods=['GET'])
 def logout():
-	session.pop('username', None)
-	return redirect(url_for('index'))
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-	if request.method == 'POST':
-		username = request.form['username']
-		password = request.form['password']
-		salt = uuid.uuid4().hex
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        salt = uuid.uuid4().hex
 
-		hashed_password = sha256((password + salt).encode()).hexdigest()
+        hashed_password = sha256((password + salt).encode()).hexdigest()
 
-		database.execute('INSERT INTO users VALUES ("{}", "{}", "{}");'.format(username, hashed_password, salt))
-		return render_template('register.html', success='User registered successfully')
-	return render_template('register.html')
+        database.execute('INSERT INTO users VALUES ("{}", "{}", "{}");'.format(username, hashed_password, salt))
+        return render_template('register.html', success='User registered successfully')
+    return render_template('register.html')
+
 
 @app.route('/trips', methods=['GET', 'POST'])
 def trips():
-	if request.method == 'POST':
-		id = uuid.uuid4()
-		username = request.form['username']
-		origin = request.form['origin']
-		destination = request.form['destination']
+    if request.method == 'POST':
+        id = uuid.uuid4()
+        username = request.form['username']
+        origin = request.form['origin']
+        destination = request.form['destination']
 
-		database.execute('INSERT INTO trips VALUES ("{}", "{}", "{}", "{}");'.format(id, username, origin, destination))
-		return render_template('trip_list', success='Trip planned successfully')
+        database.execute('INSERT INTO trips VALUES ("{}", "{}", "{}", "{}");'.format(id, username, origin, destination))
+        return render_template('trip_list', success='Trip planned successfully')
 
-	return render_template('trip_list.html')
+    return render_template('trip_list.html')
+
 
 @app.route('/trips/new', methods=['GET'])
 def trip():
-	return render_template('trip_form.html')
+    return render_template('trip_form.html')
+
 
 @app.errorhandler(404)
 def not_found(error):
     return render_template('not_found.html'), 404
+
 
 @app.errorhandler(500)
 def not_found(error):
--- database.py
+++ database.py
@@ -4,36 +4,43 @@ import sys
 DATABASE_FILE = ':memory:'
 conn = sqlite3.connect(DATABASE_FILE)
 
+
 def logger(f):
     def with_logger(*args, **kwargs):
         print("Executing query", *args, file=sys.stderr)
         return f(*args, **kwargs)
+
     return with_logger
+
 
 @logger
 def execute(query):
     with conn:
         conn.executescript(query)
 
+
 @logger
 def fetchone(query):
     with conn:
         return conn.execute(query).fetchone()
+
 
 @logger
 def fetchall(query):
     with conn:
         return conn.execute(query).fetchall()
 
+
 def init_database():
     conn.executescript("""
-    CREATE TABLE users (username text PRIMARY KEY UNIQUE NOT NULL, password text NOT NULL, salt text NOT NULL);
-    INSERT INTO users VALUES ('test', '52d42704a5b54ab6800cc2385c60c6dc62e80da8e4a98b3c6a28dd1fcaa6015a', 'e5db57d0fd6c4e6fbc4ace161beb855f');
-
-    CREATE TABLE trips (id uuid PRIMARY KEY UNIQUE NOT NULL, username text NOT NULL, origin text NOT NULL, destination text NOT NULL, seats int NOT NULL);
-
-    CREATE TABLE carpools (id uuid NOT NULL, username text NOT NULL);
+        CREATE TABLE users (username text PRIMARY KEY UNIQUE NOT NULL, password text NOT NULL, salt text NOT NULL);
+        INSERT INTO users VALUES ('test', '52d42704a5b54ab6800cc2385c60c6dc62e80da8e4a98b3c6a28dd1fcaa6015a', 'e5db57d0fd6c4e6fbc4ace161beb855f');
+    
+        CREATE TABLE trips (id uuid PRIMARY KEY UNIQUE NOT NULL, username text NOT NULL, origin text NOT NULL, destination text NOT NULL, seats int NOT NULL);
+    
+        CREATE TABLE carpools (id uuid NOT NULL, username text NOT NULL);
     """)
     conn.commit()
+
 
 init_database()
