4d2a0244b1add1997d98b2791c1a1a13f9356c8b
tryuan99 <tryuan99@gmail.com>
2018-11-03 22:39:26 -0700

    Added Maps API to trip form

--- database.py
+++ database.py
@@ -37,10 +37,10 @@ def init_database():
     conn.executescript("""
         CREATE TABLE users (username text PRIMARY KEY UNIQUE NOT NULL, password text NOT NULL, salt text NOT NULL);
         INSERT INTO users VALUES ('test', '52d42704a5b54ab6800cc2385c60c6dc62e80da8e4a98b3c6a28dd1fcaa6015a', 'e5db57d0fd6c4e6fbc4ace161beb855f');
-    
+
         CREATE TABLE trips (id uuid PRIMARY KEY UNIQUE NOT NULL, username text NOT NULL, origin text NOT NULL, destination text NOT NULL, seats int NOT NULL, fare int NOT NULL, date date NOT NULL, time time NOT NULL);
-        INSERT INTO trips VALUES ("65c8d18d-8678-4f3f-b87d-dc3e47c5c732", "test", "apple hq, cupertino", "google hq, mountain view", "4", "5", "2019-05-02", "17:20");
-    
+        INSERT INTO trips VALUES ("f78f03fe-fac6-4c60-9020-cae667f29102", "test", "Apple Campus, Cupertino, CA, USA", "Googleplex, Amphitheatre Parkway, Mountain View, CA, USA", "4", "5", "2020-05-20", "17:20");    
+
         CREATE TABLE carpools (id uuid NOT NULL, username text NOT NULL);
     """)
     conn.commit()
